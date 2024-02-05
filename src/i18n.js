c32732ae50d1861914245f47e894166a38b4f4bc
Shoha_Coder <shohacoder@gmail.com>
2024-02-05 08:35:16 +0500

    #fix #18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,22 +1,19 @@
-import i18n from "i18next";
-import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import backend from "i18next-http-backend";
+import i18n from "i18next";
 
 i18n
-  .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
+  .use(backend)
   .init({
-    fallbackLng: "uz",
+    fallbackLng: "en",
+    lng: "en",
+    backend: {
+      loadPath: "src/i18n/locales/{{lng}}.json",
+      addPath: "src/i18n/locales/add/{{lng}}",
+    },
+    interpolation: { escapeValue: false },
     debug: true,
-    detection: {
-      order: ["queryString", "cookie"],
-      cache: ["cookie"],
-    },
-    interpolation: {
-      escapeValue: false,
-    },
   });
 
 export default i18n;
