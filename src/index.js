d9b8c4aae5379b7045eee186bda346f1dd46dc11
Shoha_Coder <shohacoder@gmail.com>
2024-02-05 18:56:52 +0500

    #fix #js #json #added vercel analytics and speed insights

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Index from "./Paths/Paths.jsx";
 import { CartProvider } from "react-use-cart";
 import "./i18n";
 import { ContextProvider } from "./Context.js";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +15,8 @@ root.render(
     <BrowserRouter>
       <CartProvider>
         <Index />
+        <SpeedInsights />
+        <Analytics />
       </CartProvider>
     </BrowserRouter>
   </ContextProvider>
